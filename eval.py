2da364c9632bca5c600bf444992d2a8273882c3e
solmir36 <72752392+solmir36@users.noreply.github.com>
2020-12-22 18:58:50 +0300

    Main3

--- eval.py
+++ eval.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 # импортируйте все библиотеки, которые вам понадобятся
-
+import cv2
+import tensorflow as tf
+import numpy as np
 
 def standardize_input(image):
-    standard_im = image
-    """Приведение изображений к стандартному виду. 
+    """Приведение изображений к стандартному виду.
         Входные данные: изображение
         Выходные данные: стандартизированное изображений.
     """
+    standard_im = cv2.imread(image)
+    standard_im = cv2.cvtColor(standard_im, cv2.COLOR_BGR2GRAY)
 
     ## TODO: Если вы хотите преобразовать изображение в формат, одинаковый для всех изображений, сделайте это здесь.
     return standard_im
 
 # Пропишите путь к вашей модели
-MODEL_FILE_NAME = ''
+MODEL_FILE_NAME = './'
 
 def load_final_model():
     """ Функция осуществляет загрузку модели машинного обучения из файла MODEL_FILE_NAME.
         Выходные параметры: загруженная модель
     """
     ## TODO: Функция загрузки модели
-    return model
+    model1 = tf.keras.models.load_model("d1.h5")
+    model2 = tf.keras.models.load_model("d2.h5")
+    model3 = tf.keras.models.load_model("d3.h5")
+
+    return model1, model2, model3
 
 def predict(image, model):
     """ Функция осуществляет подачу данных на вход модели и возвращает вектор формата [1, 2]. Длина вектора может быть от 1 до 3.
@@ -28,5 +35,18 @@ def predict(image, model):
          Выходные параметры: вектор с определёнными цифрами на изображении.
     """
     ## TODO: Функция предсказания вектора цифр по заданной картинке
+    sample_shape = (480, 640)
+    image = np.reshape(image, (sample_shape[0], sample_shape[1], 1))
+    data = tf.convert_to_tensor([tf.cast(image, tf.float64) / 127.5 - 1.0])
+    digit1 = model[0].predict(data)[0][0]
+    digit2 = model[1].predict(data)[0][0]
+    digit3 = model[2].predict(data)[0][0]
+    ans = []
+    if digit1 > 0.5:
+        ans.append(1)
+    if digit2 > 0.5:
+        ans.append(2)
+    if digit3 > 0.5:
+        ans.append(3)
 
-    return vec
+    return ans
